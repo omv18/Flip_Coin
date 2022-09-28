a56c8235d06d7ad6b63aedbfdc8ea3f4728937c7
Sourav Kumar <18cse034.souravkumar@giet.edu>
2022-09-28 19:53:19 +0530

    US2 code

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "As a Simulator start with Flipping a Coin to Display Heads or Tails as winner";
-a=$((RANDOM%2));
-echo "Coin value is = " $a;
 b=0
-if [ $a == $b ]
-then 
-	echo "Head is coming"
-else 
-	echo "Tail is coming"
-fi
+echo "Coin is flip 10 times";
+for((i=0;i<10;i++))
+do	
+	a=$((RANDOM%2));
+	echo "Coin value is = " $a;
+	if [ $a == $b ]
+	then 
+		echo "Coin flip on = " $i "Head is coming"
+	else 
+		echo "Coin flip on = " $i "Tail is coming"
+	fi
+done
