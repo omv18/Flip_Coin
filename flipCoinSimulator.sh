f712787cc17269a2cf80bc32e259a7ffd0a34065
Sourav Kumar <18cse034.souravkumar@giet.edu>
2022-09-28 21:45:51 +0530

    Again commit UC3

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,31 +2,40 @@
 
 echo "As a Simulator start with Flipping a Coin to Display Heads or Tails as winner";
 b=0
-echo "Coin is flip 10 times";
-h=0
-t=0
+echo "Coin is flip 21 times";
+let h=0
+let t=0
 for((i=0;i<21;i++))
 do	
 	a=$((RANDOM%2));
-	echo "Coin value is = " $a;
 	if [ $a == $b ]
 	then 
-		$h++;
-		echo $h;
-		echo "Coin flip on = " $i "Head is coming"
+		let h=h+1
+		head[$h]=$h;
 	else 
-		$t++;
-		echo $t;
-		echo "Coin flip on = " $i "Tail is coming"
+		let t=t+1
+		tail[$t]=$t
 	fi
 done
-
-if [ h -gt t]
+echo "ARRAY FOR HEAD" ${head[@]};
+echo "SIZE FOR HEAD" ${#head[@]} ;
+z=${#head[@]};
+echo "ARRAY FOR TAIL" ${tail[@]};
+echo "ARRAY FOR TAIL" ${#tail[@]};
+y=${#tail[@]};
+echo "Head Win By VALUE =" $z;
+echo "TAIL Win By VALUE =" $y;
+if [ $z -gt $y ]
 then
- 	echo "Hail is won"
-elif [ h -eq t ]
+ 	echo "Head is won"
+	f=$(($z - $y))
+	echo "Head is won by" $f
+	
+elif [ $z -eq $y ]
 then
 	echo "Tie in between head and tail"
 else 
 	echo "Tail is won"
+	g=$(($y - $z))
+	echo "Tail is won by" $g
 fi
